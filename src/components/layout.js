0c6b8eafe597c76463adefb94f2128e8e82d7ae2
Akshaj Jagarapu <askjag07@gmail.com>
2021-05-27 15:56:46 +0530

    Fixed mounting errors.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,72 +6,81 @@ import 'bootstrap/js/dist/collapse'
 let profile = {}
 
 export default class Layout extends React.Component {
+  _isMounted = false
   componentDidMount() {
+    this._isMounted = true
     profile = JSON.parse(window.sessionStorage.getItem('profile'))
   }
   logout() {
-    window.sessionStorage.clear()
-    navigate('/app/login/', {
-      replace: true,
-    })
+    if (this._isMounted) {
+      window.sessionStorage.clear()
+      navigate('/app/login/', {
+        replace: true,
+      })
+    }
   }
   getLinks(link) {
-    let jsx
-    switch (link) {
-      default:
-        jsx = (
-          <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-            {profile.Admin && (
-              <li className="nav-item me-2">
-                <Link className="nav-link" to="/app/students/">
-                  Students
-                </Link>
-              </li>
-            )}
-            <li className="nav-item">
-              <Link className="nav-link" to="/app/resources/">
-                Resources
-              </Link>
-            </li>
-          </ul>
-        )
-        break
-      case 'Resources':
-        jsx = (
-          <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-            <li className="nav-item me-2">
-              <Link className="nav-link" to="/app/">
-                Home
-              </Link>
-            </li>
-            {profile.Admin && (
+    if (this._isMounted) {
+      let jsx
+      switch (link) {
+        default:
+          jsx = (
+            <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+              {profile.Admin && (
+                <li className="nav-item me-2">
+                  <Link className="nav-link" to="/app/students/">
+                    Students
+                  </Link>
+                </li>
+              )}
               <li className="nav-item">
-                <Link className="nav-link" to="/app/students/">
-                  Students
+                <Link className="nav-link" to="/app/resources/">
+                  Resources
                 </Link>
               </li>
-            )}
-          </ul>
-        )
-        break
-      case 'Students':
-        jsx = (
-          <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-            <li className="nav-item me-2">
-              <Link className="nav-link" to="/app/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/app/resources/">
-                Resources
-              </Link>
-            </li>
-          </ul>
-        )
-        break
+            </ul>
+          )
+          break
+        case 'Resources':
+          jsx = (
+            <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+              <li className="nav-item me-2">
+                <Link className="nav-link" to="/app/">
+                  Home
+                </Link>
+              </li>
+              {profile.Admin && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/app/students/">
+                    Students
+                  </Link>
+                </li>
+              )}
+            </ul>
+          )
+          break
+        case 'Students':
+          jsx = (
+            <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+              <li className="nav-item me-2">
+                <Link className="nav-link" to="/app/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/app/resources/">
+                  Resources
+                </Link>
+              </li>
+            </ul>
+          )
+          break
+      }
+      return jsx
     }
-    return jsx
+  }
+  componentWillUnmount() {
+    this._isMounted = false
   }
   render() {
     const { children, link } = this.props
