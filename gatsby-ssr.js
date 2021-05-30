7b85b6b96ba8cebf63557cd2c96a15109a5aa7bc
Akshaj Jagarapu <askjag07@gmail.com>
2021-05-30 21:09:02 +0530

    Ran lighthouse tests.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-export const onRenderBody = function ({ setPostBodyComponents }) {
+export const onRenderBody = function ({
+  setHtmlAttributes,
+  setPostBodyComponents,
+}) {
+  setHtmlAttributes({
+    lang: 'en',
+  })
   setPostBodyComponents([
     <script
       key="https://static.opentok.com/v2/js/opentok.min.js"
